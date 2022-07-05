f0345526785008b275a05a116843c2afa9fa6bb6
Kerish <100kirill24@gmail.com>
2022-07-05 18:27:23 +0500

    header - 2, banner - 1

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,13 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+  let header= $('.header');
 
-})
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 400) {
+      header.addClass('header_fixed');
+    } else { 
+      header.removeClass('header_fixed');
+    }
+  });
+});
